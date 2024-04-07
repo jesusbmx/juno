c63e7c6d4f25327f6dd837a0e1dd7ad07c5323ce
JesusBMX <jjbaezmm@hotmail.com>
2024-04-07 14:31:32 -0600

    Dictionary

--- src/juno/content/Dictionary.java
+++ src/juno/content/Dictionary.java
@@ -1,10 +1,25 @@
 package juno.content;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 public class Dictionary implements Cloneable {
 
@@ -22,7 +37,8 @@ public class Dictionary implements Cloneable {
         this(new LinkedHashMap<String, Object>(dictionary.values));
     }
     
-    public void putAll(Dictionary dict) {
+    
+    public void setAll(Dictionary dict) {
         values.putAll(dict.values);
     }
     
@@ -220,25 +236,142 @@ public class Dictionary implements Cloneable {
         return toString(0);
     }
     
+    
+    public synchronized void load(InputStream in) throws IOException {
+        try {
+            final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            final Document doc = dBuilder.parse(new InputSource(new InputStreamReader(in)));
+            doc.getDocumentElement().normalize();
 
-//    public static void main(String[] args) {
+            parseNode(doc.getDocumentElement(), this);
+
+        } catch (Exception e) {
+            throw new IOException("Error al leer el archivo XML", e);
+        } 
+    }
+    
+    private static void parseNode(Node node, Dictionary dictionary) {
+        final NodeList nodeList = node.getChildNodes();
+        
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            final Node childNode = nodeList.item(i);
+            
+            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                final Element element = (Element) childNode;
+                final String nodeName = element.getNodeName();
+                final String value = element.getTextContent();
+                
+                if (!nodeName.isEmpty()) {
+                    if ("String".equals(nodeName)) {
+                        final String key = element.getAttribute("key");
+                        dictionary.setString(key, value);
+                    } else if ("Integer".equals(nodeName)) {
+                        final String key = element.getAttribute("key");
+                        dictionary.setInt(key, Integer.parseInt(value));
+                    } else if ("Float".equals(nodeName)) {
+                        final String key = element.getAttribute("key");
+                        dictionary.setFloat(key, Float.parseFloat(value));
+                    } else if ("Long".equals(nodeName)) {
+                        final String key = element.getAttribute("key");
+                        dictionary.setLong(key, Long.parseLong(value));
+                    } else if ("Boolean".equals(nodeName)) {
+                        final String key = element.getAttribute("key");
+                        dictionary.setBoolean(key, Boolean.parseBoolean(value));
+                    } else if ("Dictionary".equals(nodeName)) {
+                        final String key = element.getAttribute("key");
+                        final Dictionary subDict = new Dictionary();
+                        parseNode(childNode, subDict);
+                        dictionary.setDictionary(key, subDict);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void store(OutputStream out) throws IOException {
+        try {
+            final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            final Document doc = docBuilder.newDocument();
+
+            // Creamos un elemento raíz
+            final Element rootElement = doc.createElement("Dictionary");
+            doc.appendChild(rootElement);
+
+            // Pasamos el elemento raíz a writeDictionaryToXML
+            writeDictionaryToXML(this, doc, rootElement);
+
+            final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            final Transformer transformer = transformerFactory.newTransformer();
+            final DOMSource source = new DOMSource(doc);
+            final StreamResult result = new StreamResult(out);
+            
+            transformer.transform(source, result);
+            
+        } catch (Exception e) {
+            throw new IOException("Error al escribir en el archivo XML", e);
+        }
+    }
+
+    private static void writeDictionaryToXML(Dictionary dictionary, Document doc, Element parentElement) {
+        for (Map.Entry<String, Object> entry : dictionary.entrySet()) {
+            final String key = entry.getKey();
+            final Object value = entry.getValue();
+            if (value == null || key.isEmpty()) {
+                continue;
+            }
+
+            final Element element;
+            if (value instanceof Dictionary) {
+                element = doc.createElement("Dictionary");
+                element.setAttribute("key", key);
+                writeDictionaryToXML((Dictionary) value, doc, element);
+            } else {
+                element = doc.createElement(value.getClass().getSimpleName());
+                element.setAttribute("key", key);
+                element.setTextContent(value.toString());
+            }
+            parentElement.appendChild(element);
+        }
+    }
+
+ 
+
+//    public static void main(String[] args) throws IOException {
+//        final File file = new File("Dictionary.xml");
+//        
 //        final Dictionary dict = new Dictionary();
+//        if (file.exists()) {
+//            dict.load(new FileInputStream(file));
+//        }
+//        
 //        dict.setString("null", null);
 //        dict.setString("string", "Hola mundo");
 //        dict.setInt("int", 10);
 //        dict.setFloat("float", 10.2f);
 //        dict.setLong("long", System.currentTimeMillis());
 //        dict.setBoolean("bool", true);
-//        final Dictionary clone = dict.clone();
 //        
 //        final Dictionary subdict1 = new Dictionary();
-//        subdict1.putAll(clone);
+//        subdict1.setString("null", null);
+//        subdict1.setString("string", "Hola mundo");
+//        subdict1.setInt("int", 10);
+//        subdict1.setFloat("float", 10.2f);
+//        subdict1.setLong("long", System.currentTimeMillis());
+//        subdict1.setBoolean("bool", true);
 //        dict.setDictionary("subdict1", subdict1);
 //        
 //        final Dictionary subdict2 = new Dictionary();
-//        subdict2.putAll(clone);
+//        subdict2.setString("null", null);
+//        subdict2.setString("string", "Hola mundo");
+//        subdict2.setInt("int", 10);
+//        subdict2.setFloat("float", 10.2f);
+//        subdict2.setLong("long", System.currentTimeMillis());
+//        subdict2.setBoolean("bool", true);
 //        subdict1.setDictionary("subdict2", subdict2);
 //        
+//        dict.store(new FileOutputStream(file));
 //        System.out.println(dict);
 //    }
 }
