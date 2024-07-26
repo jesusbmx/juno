df8d722c42b59a835aced39abc66e7d132801ef8
JesusBMX <jjbaezmm@hotmail.com>
2024-07-26 08:59:52 -0600

    Refactor Paths: init

---
+++ src/juno/io/Paths.java
@@ -0,0 +1,22 @@
+
+package juno.io;
+
+import java.io.File;
+
+public class Paths {
+    
+    public static String join(char separator, Object[] args) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            if (i > 0 && sb.charAt(sb.length() - 1) != separator) {
+                sb.append(separator);
+            }
+            sb.append(args[i]);
+        }
+        return sb.toString();
+    }
+    
+    public static String join(Object... args) {
+        return join(File.separatorChar, args);
+    }
+}
